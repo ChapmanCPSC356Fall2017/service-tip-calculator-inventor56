1fce5b1e36322aac34253e861cb01e66d124ceb3
Josh Smith <joshner.smith@gmail.com>
2017-09-08 23:42:03 -0700

    - 1st commit

---
+++ SmithTipCalculator/app/src/main/java/com/example/jsjos/smithtipcalculator/CalcTip.java
@@ -0,0 +1,41 @@
+package com.example.jsjos.smithtipcalculator;
+
+/**
+ * Created by jsjos on 9/7/2017.
+ */
+
+public class CalcTip {
+
+    public static double getTip(double price, int starRating) {
+        double percentage = 0;
+        if (starRating >= 1 && starRating <= 3) {
+            percentage = 0.1;
+        }
+        else if (starRating >= 4 && starRating <= 5) {
+            percentage = 0.13;
+        }
+        else if (starRating >= 6 && starRating <= 7) {
+            percentage = 0.15;
+        }
+        else if (starRating >= 8 && starRating <= 9) {
+            percentage = 0.20;
+        }
+        else if (starRating == 10) {
+            percentage = 0.25;
+        }
+        // Calculate and Return Computed Tip
+
+        return Math.round((price * percentage) * 100) / 100;
+    }
+
+    public static boolean isDefined(String inputStr) {
+        try {
+            double test = Double.parseDouble(inputStr);
+
+        }
+        catch (NumberFormatException exc) {
+            return false;
+        }
+        return true;
+    }
+}
--- SmithTipCalculator/app/src/main/java/com/example/jsjos/smithtipcalculator/MainActivity.java
+++ SmithTipCalculator/app/src/main/java/com/example/jsjos/smithtipcalculator/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.jsjos.smithtipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView enterText;
+    private EditText enterTotal;
+    private EditText serviceRatingHint;
+    private Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.enterText = (TextView) findViewById(R.id.starRating);
+        this.enterTotal = (EditText) findViewById(R.id.totalHint);
+        this.serviceRatingHint = (EditText) findViewById(R.id.serviceRatingHint);
+        this.submitButton = (Button) findViewById(R.id.submitButton);
+
+        this.submitButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // Check to make sure input is valid
+                if (
+                        CalcTip.isDefined(enterTotal.getText().toString()) &&
+                        CalcTip.isDefined(serviceRatingHint.getText().toString())
+                        )
+                {
+
+                }
+
+                // Calculate Tip
+
+            }
+
+
+        });
+
+
+
     }
 }
